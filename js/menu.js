b9ee5dc3f19f42bebccd552b882202956fb6eb9f
Mohamed Aden Ighe <80690364+katarighe@users.noreply.github.com>
2023-06-26 17:58:55 +0300

    Add new JS function of the mobile menu

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById('myNav').style.width = '100%';
+  }
+  
+  function closeNav() {
+    document.getElementById('myNav').style.width = '0%';
+  }
+  
+  openNav();
+  closeNav();
+  
